72ff1020fe33ce068ab66347c4f23116d41650d9
Veer <19veert@gmail.com>
2026-06-30 20:10:19 +0530

    Improve hashmap string hashing

--- src/utils/common.h
+++ src/utils/common.h
@@ -7,11 +7,12 @@
 #define container_of(ptr, type, member) \
     ((type *)((char *)(ptr) - offsetof(type, member)))
 
-// FNV-1a 32-bit hash, widened to uint64_t for hashmap compatibility.
+// FNV-1a 64-bit hash for hashmap keys.
 inline uint64_t str_hash(const uint8_t *data, size_t len) {
-    uint32_t h = 0x811C9DC5;
+    uint64_t h = 14695981039346656037ULL;
     for (size_t i = 0; i < len; i++) {
-        h = (h + data[i]) * 0x01000193;
+        h ^= data[i];
+        h *= 1099511628211ULL;
     }
     return h;
 }
